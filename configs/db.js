3b71978e33a858008413fdc6f4ce13b91c1459a9
Danh Huynh <huynhthanhdanh9a4@gmail.com>
2025-09-10 14:13:00 +0700

    update security fix db

--- configs/db.js
+++ configs/db.js
@@ -1,17 +1,17 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    // user: "postgresqlcodingservicedb_user",
-    // host: "dpg-d304r3n5r7bs73b1jg6g-a",
-    // database: "postgresqlcodingservicedb",
-    // password: "sAMfQAct2KgKzZeTnerhwFrmYKBu4m5W",
-    // port: 5432
-
-    user: "postgres",
-    host: "localhost",
-    database: "codingservicedb",
-    password: "phungsu123",
+    user: "postgresqlcodingservicedb_user",
+    host: "dpg-d304r3n5r7bs73b1jg6g-a",
+    database: "postgresqlcodingservicedb",
+    password: "sAMfQAct2KgKzZeTnerhwFrmYKBu4m5W",
     port: 5432
+
+    // user: "postgres",
+    // host: "localhost",
+    // database: "codingservicedb",
+    // password: "phungsu123",
+    // port: 5432
 });
 
 module.exports = pool;
