1b17479bc3b41c2c459581f3813fd6a24a691e2c
Danh Huynh <huynhthanhdanh9a4@gmail.com>
2025-09-10 12:43:59 +0700

    fix push server

--- configs/db.js
+++ configs/db.js
@@ -1,16 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    // user: "postgresqlcodingservicedb_user",
-    // host: "dpg-d304r3n5r7bs73b1jg6g-a",
-    // database: "postgresqlcodingservicedb",
-    // password: "sAMfQAct2KgKzZeTnerhwFrmYKBu4m5W",
-    // port: 5432
-
-    user: "postgres",
-    host: "localhost",
-    database: "codingservicedb",
-    password: "phungsu123",
+    user: "postgresqlcodingservicedb_user",
+    host: "dpg-d304r3n5r7bs73b1jg6g-a",
+    database: "postgresqlcodingservicedb",
+    password: "sAMfQAct2KgKzZeTnerhwFrmYKBu4m5W",
     port: 5432
 });
 
